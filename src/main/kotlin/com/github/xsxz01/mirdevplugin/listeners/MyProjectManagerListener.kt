fe5a854440da29be90784a296334bfb8ad6eba2f
GitHub Action <action@github.com>
2023-02-12 15:58:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/xsxz01/mirdevplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/xsxz01/mirdevplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.xsxz01.mirdevplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xsxz01.mirdevplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
